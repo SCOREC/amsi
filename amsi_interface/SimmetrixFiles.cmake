0b40a1e0da5143b17bbbff111a0a61c920a15605
William Tobin <tobinw2@rpi.edu>
2015-07-08 14:50:27 -0400

    adding Elasticity files to Simmetrix-only files

--- amsi_interface/SimmetrixFiles.cmake
+++ amsi_interface/SimmetrixFiles.cmake
@@ -2,9 +2,11 @@ list(APPEND interface_public_headers
   SimFEA.h
   apfSimFEA.h
   SimTensorFieldQuery.h
-  simBCDirichlet.h)
+  simBCDirichlet.h
+  Elasticity.h)
 
 list(APPEND interface_src_files
   SimFEA.cc
   apfSimFEA.cc
-  simBCDirichlet.cc)
+  simBCDirichlet.cc
+  Elasticity.cc)
