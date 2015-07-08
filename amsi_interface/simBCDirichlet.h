c88b3b4825942434671686f7099a56d557691d9a
William Tobin <tobinw2@rpi.edu>
2015-07-08 11:14:26 -0400

    initial implementation of a replacement for the apfSimFEA method of applying dirichlet boundary conditions, using the slightly more general method developed yesterday

---
+++ amsi_interface/simBCDirichlet.h
@@ -0,0 +1,15 @@
+#ifndef SIMBCDIRICHLET_H_
+#define SIMBCDIRICHLET_H_
+
+#include "amsiBCDirichlet.h"
+
+namespace amsi {
+
+  void buildDirichletSpecFromSIM(pGModel model,
+				 const std::string & attribute_name,
+				 const std::string & constraint_name
+				 int component_offset,
+				 std::vector<DirichletSpecification> & spec);
+}
+
+#endif
