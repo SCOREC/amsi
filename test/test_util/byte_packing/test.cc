ae019215c4aafcb19a023e772094bfe4703e7df3
William Tobin <tobinw2@rpi.edu>
2015-08-21 13:44:16 -0400

    added pod struct test for byte packing test case

--- test/test_util/byte_packing/test.cc
+++ test/test_util/byte_packing/test.cc
@@ -11,16 +11,15 @@
 
 //using namespace AMSI;
 
+struct pod_struct {
+  int int_val;
+  char char_val;
+  double double_val;
+};
+
 int main(int argc, char * argv[])
 {
   int result = 0;
-  MPI_Init(&argc,&argv);
-
-  int rank = -1;
-  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-
-  int size;
-  MPI_Comm_size(MPI_COMM_WORLD,&size);
 
   int int_val = 6;
   double double_val = 3.14;
@@ -34,12 +33,27 @@ int main(int argc, char * argv[])
   byte_unpack(data,recovered_int,recovered_double);
   free(data);
 
+  pod_struct struct_val;
+  struct_val.int_val = 16;
+  struct_val.char_val = 'g';
+  struct_val.double_val = 6.28;
+
+  void * data2 = talloc(struct_val);
+  byte_pack(data2,struct_val);
+
+  pod_struct recovered_struct;
+  byte_unpack(data2,recovered_struct);
+  free(data2);
+
 //  result += strcmp(char_string,recovered_string);
   result += test("recover integer",int_val,recovered_int);
   result += test("recover double",double_val,recovered_double);
+  result += test("recover struct int",struct_val.int_val,recovered_struct.int_val);
+  result += test("recover struct char",struct_val.char_val,recovered_struct.char_val);
+  result += test("recover struct double",struct_val.double_val,recovered_struct.double_val);
 
-  std::cout << result << std::endl;
+  std::cout << "Errors: " << result << std::endl;
 
-  MPI_Finalize();
+//  MPI_Finalize();
   return result;
 }
