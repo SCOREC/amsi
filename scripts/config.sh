65f4cadd3ba4eb8eac6c435f1febfef9636256d2
William Tobin <tobinw2@rpi.edu>
2015-09-15 13:59:32 -0400

    adding missing lines to determine current build type

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,8 +4,22 @@
 # usage: ./config.sh [build_type]
 #
 
+if [ -z $1 ]; then
+  BUILD_TYPE=Debug
+else
+  BUILD_TYPE=$1
+fi
 
+if [ "$BUILD_TYPE" == "Debug" ]; then
+  BUILD_DIR=../build_debug
+elif [ "$BUILD_TYPE" == "Release" ] ; then
+  BUILD_DIR=../build_release
+fi
 
+if [ ! -d $BUILD_DIR ]; then
+  mkdir $BUILD_DIR
+fi
+cd $BUILD_DIR
 rm -rf ./* #stupid and dangerous
 
 module load cmake
