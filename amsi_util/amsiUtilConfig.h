fca0f4d5b9509daf0ef638e736d7f7ab9ef33e65
Victor Chan <chanv3@mastermind.scorec.rpi.edu>
2015-10-13 13:10:03 -0400

    added to match with repository.

---
+++ amsi_util/amsiUtilConfig.h
@@ -0,0 +1,28 @@
+#ifndef AMSIUTILCONFIG_H_IN_
+#define AMSIUTILCONFIG_H_IN_
+
+#define AMSI_MAJOR_VERSION 1
+#define AMSI_MINOR_VERSION 0
+#define AMSI_PATCH_VERSION 0
+
+#define VALUE_TO_STRING(x) #x
+#define VALUE(x) VALUE_TO_STRING(x)
+#define VAR_NAME_VALUE(var) #var "="  VALUE(var)
+
+#define SCOREC
+
+#if TRUE == TRUE
+#  define CORE
+#endif
+
+#if TRUE == TRUE
+#  define SIM
+#endif
+
+#ifndef NDEBUG
+#  define AMSI_DEBUG(EX)
+#else 
+#  define AMSI_DEBUG(EX) EX
+#endif
+
+#endif
