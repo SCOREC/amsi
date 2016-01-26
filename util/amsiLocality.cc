b529fbe1e92eca0be044d0433500287fea3566af
William Tobin <tobinw2@rpi.edu>
2016-01-26 10:20:26 -0500

    reworked the machine locality interface and documented

--- util/amsiLocality.cc
+++ util/amsiLocality.cc
@@ -10,11 +10,30 @@ namespace amsi
     char node_nm[MPI_MAX_PROCESSOR_NAME] = {};
     int nm_len = 0;
     MPI_Get_processor_name(node_nm, &nm_len);
-    std::string nm_str(node_nm);
-    lid = generateUUID(nm_str);
+    //std::string nm_str(node_nm);
+    lid = generateUUID(std::string(node_nm));
     return lid;
   }
-  void getRanksOnNode(MPI_Comm cm, uuid n_id, RankSet & rnks)
+  void getNodeSet(std::vector<uuid> & nds, MPI_Comm cm)
+  {
+    int sz = 0;
+    MPI_Comm_size(cm,&sz);
+    int rnk = -1;
+    MPI_Comm_rank(cm,&rnk);
+    nds.resize(sz);
+    uuid n_id = getNodeID();
+    nds[rnk] = n_id;
+    MPI_Allgather(&n_id,1,mpi_type<uuid>(),
+                  &nds[0],1,mpi_type<uuid>(),
+                  cm);
+    std::sort(nds.begin(), nds.end());
+    std::vector<uuid>::iterator ne = std::unique(nds.begin(), nds.end());
+    nds.erase(ne,nds.end());
+  }
+  void getRanksOnSocket(uuid n_id, RankSet & rnks, MPI_Comm cm)
+  {
+  }
+  void getRanksOnNode(uuid n_id, RankSet & rnks, MPI_Comm cm)
   {
     // init
     int sz = 0;
@@ -35,29 +54,10 @@ namespace amsi
       it++;
     }
   }
-  void getRanksOnSocket(MPI_Comm cm, uuid n_id, RankSet & rnks)
-  {
-  }
-  void getNodeSet(MPI_Comm cm, std::vector<uuid> & nds)
-  {
-    int sz = 0;
-    MPI_Comm_size(cm,&sz);
-    int rnk = -1;
-    MPI_Comm_rank(cm,&rnk);
-    nds.resize(sz);
-    uuid n_id = getNodeID();
-    nds[rnk] = n_id;
-    MPI_Allgather(&n_id,1,mpi_type<uuid>(),
-                  &nds[0],1,mpi_type<uuid>(),
-                  cm);
-    std::sort(nds.begin(), nds.end());
-    std::vector<uuid>::iterator ne = std::unique(nds.begin(), nds.end());
-    nds.erase(ne,nds.end());
-  }
   rank_t getNthRankOnNode(uuid nid, rank_t nth, MPI_Comm cm)
   {
     DefaultRankSet rnks;
-    getRanksOnNode(cm,nid,rnks);
+    getRanksOnNode(nid,rnks,cm);
     std::vector<rank_t> on_nd;
     pushTo(&rnks,std::back_inserter(on_nd));
     assert((unsigned)nth < on_nd.size());
--- util/amsiLocality.h
+++ util/amsiLocality.h
@@ -8,15 +8,60 @@
 namespace amsi
 {
   extern hwloc_topology_t machine_topology;
-  /// brief : get a uuid associated with the hardware processor associated with the local mpi rank
-  uuid getNodeID();
-  // brief : get all mpi ranks in comm associated with the hardware processor p_id
   class RankSet;
-  // todo (m) : consider moving MPI_Comm to the last parameter and having a default value of AMSI_COMM_WORLD
-  void getRanksOnSocket(MPI_Comm c, uuid p_id, RankSet & rnks);
-  void getRanksOnNode(MPI_Comm c, uuid n_id, RankSet & ranks);
-  void getNodeSet(MPI_Comm c, std::vector<uuid> & nodes);
-  void getNthRankOnNode(uuid n_id, rank_t nth, rank_t rnk, MPI_Comm = AMSI_COMM_WORLD);
-  void getNthRankOnNodes(const std::vector<uuid> & nids, rank_t nth, RankSet * rnks, MPI_Comm = AMSI_COMM_WORLD);
+  /**
+   * Get a uuid (misnomer) which identifies the hardware processor
+   *  the local mpi rank is executing on. This is based on hashing
+   *  MPI_Get_processor_name which by the standard gives a unique
+   *  identifying string, so barring a hash conflict this should be
+   *  fine.
+   * @todo Should this be getSocketID?
+   */
+  uuid getNodeID();
+  /**
+   * Get a vector containing the uuid of every hardware processor
+   *  in the specified comm in no particular order.
+   * @param nodes A vector containing the set of unique hardware processors
+   *              belonging to the specified comm. Should be empty when passed.
+   * @param c The mpi comm over which to perform the operation
+   * @note This call is collective over the specified comm.
+   * @todo Should this be getSocketSet?
+   */
+  void getNodeSet(std::vector<uuid> & nodes, MPI_Comm c = AMSI_COMM_WORLD);
+  // brief : get all mpi ranks in comm associated with the hardware processor p_id
+  /**
+   * Get the mpi ranks of the processes executing on the specific
+   * hardware processor specified.
+   * @note Currently unimplemented
+   * @todo implement!
+   */
+  void getRanksOnSocket(uuid p_id, RankSet & rnks, MPI_Comm c = AMSI_COMM_WORLD);
+  /**
+   * Get the mpi ranks (in the specified comm) of the processes executing
+   *  on the specific machine node specified.
+   * @param n_id The id of the node to retrieve the ranks executing on.
+   * @param ranks The set of ranks on the specified machine node.
+   * @param c The mpi comm in which the ranks are specified.
+   */
+  void getRanksOnNode(uuid n_id, RankSet & ranks, MPI_Comm c = AMSI_COMM_WORLD);
+  /**
+   * Get the nth rank on the specified machine node in the specified comm.
+   * @return The mpi rank of the nth process on machine node n_id
+   */
+  rank_t getNthRankOnNode(uuid n_id,
+                          rank_t nth,
+                          rank_t rnk,
+                          MPI_Comm c = AMSI_COMM_WORLD);
+  /**
+   * Get the nth rank of every machine node specified in the specified comm.
+   * @param nids The machine nodes to get the mpi rank of the nth local process on.
+   * @param nth The local machine node process to retrieve the mpi rank of.
+   * @param rnks The resulting set of mpi ranks.
+   * @param c The MPI_Comm the resulting ranks are valid in.
+   */
+  void getNthRankOnNodes(const std::vector<uuid> & nids,
+                         rank_t nth,
+                         RankSet * rnks,
+                         MPI_Comm c = AMSI_COMM_WORLD);
 }
 #endif
