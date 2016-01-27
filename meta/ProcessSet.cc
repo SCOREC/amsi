13adc4cafacae62f919f4c290418aa946f626ce1
William Tobin <tobinw2@rpi.edu>
2016-01-27 11:05:10 -0500

    cleanup of process set code, and examination of responsibilities of the process set for eventual removal in lieu of the new RankSet designed for dynamic process reallocation

--- meta/ProcessSet.cc
+++ meta/ProcessSet.cc
@@ -1,73 +1,61 @@
 #include "ProcessSet.h"
-
 #include <iostream>
-
-namespace amsi {
-
-    /// @brief Default constructor for a range ProcessSet, takes low and high ranks
-    /// @param l The first process rank in the set
-    /// @param h One over the last process rank in the set
-    ProcessSet_T<std::pair<int,int> >::ProcessSet_T(int l, int h) :
-      val(std::make_pair(l,h))
-    {}
-
-
-    ProcessSet_T<std::pair<int,int> >::ProcessSet_T(std::pair<int,int> r) :
-      val(r)
-    {}
-
-    /// @brief Constructor taking a std::pair<int,int> range instead of high and low values
-    /// @param r The pair representing the range for the process set
-    ProcessSet_T<std::pair<int,int> >::ProcessSet_T(std::pair<int,int> & r) :
-      val(r)
-    {}
-    
-    /// @brief Retrieve process ranks from the set
-    /// @param i The index of the rank the retrieve
-    /// @return int The i-th rank in the set
-    int ProcessSet_T<std::pair<int,int> >::operator[](int i) const
+namespace amsi
+{
+  /// @brief Default constructor for a range ProcessSet, takes low and high ranks
+  /// @param l The first process rank in the set
+  /// @param h One over the last process rank in the set
+  ProcessSet_T<std::pair<int,int> >::ProcessSet_T(int l, int h)
+    : val(std::make_pair(l,h))
+  {}
+  ProcessSet_T<std::pair<int,int> >::ProcessSet_T(std::pair<int,int> r)
+    : val(r)
+  {}
+  /// @brief Constructor taking a std::pair<int,int> range instead of high and low values
+  /// @param r The pair representing the range for the process set
+  ProcessSet_T<std::pair<int,int> >::ProcessSet_T(std::pair<int,int> & r)
+    : val(r)
+  {}
+  /// @brief Retrieve process ranks from the set
+  /// @param i The index of the rank the retrieve
+  /// @return int The i-th rank in the set
+  int ProcessSet_T<std::pair<int,int> >::operator[](int i) const
+  {
+    return i < 0 ? -1 : i <= (val.second - val.first) ? val.first + i : -1;
+  }
+  /// @brief How many ranks are in the set
+  /// @return int The set size
+  int ProcessSet_T<std::pair<int,int> >::size() const
+  {
+    //std::cout << val.first << " " << val.second << std::endl;
+    return val.second - val.first;
+  }
+  /// @brief Determine whether a rank is in the set
+  /// @param p The rank to check
+  /// @return bool Whether the rank is in the set or not
+  bool ProcessSet_T<std::pair<int,int> >::isIn(int p) const
+  {
+    return p >= val.first && p < val.second;
+  }
+  /// @brief Extract a subset of this ProcessSet consisting of s ranks
+  /// @param s How many ranks to include in the new set
+  /// @return ProcessSet* The newly-created ProcessSet
+  ProcessSet * ProcessSet_T<std::pair<int,int> >::extract(int s)
+  {
+    ProcessSet * result = NULL;
+    if(s <= size())
     {
-      
-      return i < 0 ? -1 : i <= (val.second - val.first) ? val.first + i : -1;
+      result =
+        static_cast<ProcessSet*>(new ProcessSet_T<std::pair<int,int> >((*this)[0],(*this)[s]));
+      val.first += s;
     }
-    
-    /// @brief How many ranks are in the set
-    /// @return int The set size
-    int ProcessSet_T<std::pair<int,int> >::size() const
-    {
-      //std::cout << val.first << " " << val.second << std::endl;
-      return val.second - val.first;
-    }
-    
-    /// @brief Determine whether a rank is in the set
-    /// @param p The rank to check
-    /// @return bool Whether the rank is in the set or not
-    bool ProcessSet_T<std::pair<int,int> >::isIn(int p) const
-    {
-      return p >= val.first && p < val.second;
-    }
-
-    /// @brief Extract a subset of this ProcessSet consisting of s ranks
-    /// @param s How many ranks to include in the new set
-    /// @return ProcessSet* The newly-created ProcessSet
-    ProcessSet * ProcessSet_T<std::pair<int,int> >::extract(int s)
-    {
-      ProcessSet * result = NULL;  
-      if(s <= size())
-      {
-	result =
-	  static_cast<ProcessSet*>(new ProcessSet_T<std::pair<int,int> >((*this)[0],(*this)[s]));
-	val.first += s;
-      }
-      return result;
-    }
-
-    int ProcessSet_T<std::pair<int,int> >::indexOf(int r) const
-    {
-      int result = -1;
-      if(isIn(r))
-	result = r - val.first;
-      return result;
-    }
-
-} // namespace amsi
+    return result;
+  }
+  int ProcessSet_T<std::pair<int,int> >::indexOf(int r) const
+  {
+    int result = -1;
+    if(isIn(r))
+      result = r - val.first;
+    return result;
+  }
+}
