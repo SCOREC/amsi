d76c5702833633de38559bf2970e3898e7963c77
William Tobin <tobinw2@rpi.edu>
2016-08-16 14:00:04 -0400

    parallel volume calculation

--- interface/apfsimWrapper.cc
+++ interface/apfsimWrapper.cc
@@ -34,47 +34,51 @@ namespace amsi
      * pGFace ent:     face entity of geometric model.
      * pMesh msh:      partitioned mesh.
      * apf::Field * u: displacement field. */
-    double vol = 0.0;
+     double vol = 0.0;
     /** Extract face entities of mesh that lie on face entities of geometric model */
-    std::list<pEntity> ents;
-    getClassifiedEnts(msh,pGF,2,std::back_inserter(ents));
+     std::list<pEntity> ents;
+     getClassifiedEnts(msh,pGF,2,std::back_inserter(ents));
     /** for debugging purposes */
-    AMSI_DEBUG(
+     AMSI_DEBUG (
       std::vector<int> tags;
       int ent_size = 0;
       for (int ii=ent_size; ii<ents.size(); ii++)
       {
-	tags.push_back(GEN_tag(pGF));
-	ent_size = ents.size();
+        tags.push_back(GEN_tag(pGF));
+        ent_size = ents.size();
       }
       )
-    int surf_elem = 0;
-    for(std::list<pEntity>::iterator ent = ents.begin(); ent != ents.end(); ++ent)
-    {
-      pRegion mshRgn0 = F_region((pFace)(*ent),0);
-      pRegion mshRgn1 = F_region((pFace)(*ent),1);
-      int GRgn0_tag, GRgn1_tag, normal_dir;
-      if (mshRgn0 != NULL)
-	GRgn0_tag = GEN_tag(R_whatIn(mshRgn0));
-      else
-	GRgn0_tag = -2;
-      if (mshRgn1 != NULL)
-	GRgn1_tag = GEN_tag(R_whatIn(mshRgn1));
-      else
-	GRgn1_tag = -2;
-      if (GRgn0_tag == tag)
-	normal_dir = 1;
-      else
-	normal_dir = -1;
-  
-      AMSI_DEBUG(
-	std::cout<<"surf_elem:"<<surf_elem<<" belongs to face: "<<tags[surf_elem]
-		 <<" Rgn "<<GRgn0_tag<<" inside,"
-		 <<" Rgn "<<GRgn1_tag<<" outside,"
-		 <<" normal direction "<<normal_dir<<std::endl;
-	)
-      vol += measureDisplacedFromSurf(apf::castEntity(*ent),u,normal_dir);
-      surf_elem++;
+     int surf_elem = 0;
+     apf::Mesh * apfmsh = apf::getMesh(u);
+     for(std::list<pEntity>::iterator ent = ents.begin(); ent != ents.end(); ++ent)
+     {
+      if(apfmsh->isOwned(apf::castEntity(*ent)))
+      {
+        pRegion mshRgn0 = F_region((pFace)(*ent),0);
+        pRegion mshRgn1 = F_region((pFace)(*ent),1);
+        int GRgn0_tag, GRgn1_tag, normal_dir;
+        if (mshRgn0 != NULL)
+          GRgn0_tag = GEN_tag(R_whatIn(mshRgn0));
+        else
+          GRgn0_tag = -2;
+        if (mshRgn1 != NULL)
+          GRgn1_tag = GEN_tag(R_whatIn(mshRgn1));
+        else
+          GRgn1_tag = -2;
+        if (GRgn0_tag == tag)
+          normal_dir = 1;
+        else
+          normal_dir = -1;
+
+        AMSI_DEBUG (
+          std::cout<<"surf_elem:"<<surf_elem<<" belongs to face: "<<tags[surf_elem]
+          <<" Rgn "<<GRgn0_tag<<" inside,"
+          <<" Rgn "<<GRgn1_tag<<" outside,"
+          <<" normal direction "<<normal_dir<<std::endl;
+          )
+        vol += measureDisplacedFromSurf(apf::castEntity(*ent),u,normal_dir);
+        surf_elem++;
+      }
     }
     vol = amsi::comm_sum(vol);
     return vol;
