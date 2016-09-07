19894819747d802f65a83d0685642605afb88575
William Tobin <tobinw2@rpi.edu>
2016-09-07 18:03:04 -0400

    working volume calculation test-case

--- test/interface/analysis/volume.cc
+++ test/interface/analysis/volume.cc
@@ -1,22 +1,24 @@
+#include "amsiInterface.h"
 #include "apfsimWrapper.h"
 #include "simWrapper.h"
 #include <cstdlib>
-int main(int agrc, char * argv[])
+int main(int argc, char * argv[])
 {
   int result = 0;
   amsi::use_simmetrix = true;
   amsi::interfaceInit(argc,argv);
   pGModel mdl = GM_load(argv[1],0,NULL);
-  pParMesh msh = PM_load(argv[2],sthreadnone,mdl,NULL);
+  pParMesh msh = PM_load(argv[2],sthreadNone,mdl,NULL);
   int tg = atoi(argv[3]);
   pMesh prt = PM_mesh(msh,0);
-  apf::Field * u =  apf::createLagrangeField(msh,"u",apf::VECTOR,1);
+  apf::Mesh * apf_msh = apf::createMesh(msh);
+  apf::Field * u =  apf::createLagrangeField(apf_msh,"u",apf::VECTOR,1);
   pGRegion rgn = (pGRegion)GM_entityByTag(mdl,3,tg);
   std::vector<pGEntity> fcs;
-  amsi::cutPaste<pGEntity>(GEN_faces((pGEntity)rgn),fcs);
+  amsi::cutPaste<pGEntity>(GEN_faces((pGEntity)rgn),std::back_inserter(fcs));
   double vol = 0.0;
   for(std::vector<pGEntity>::iterator fc = fcs.begin(); fc != fcs.end(); ++fc)
-    vol += measureVol_pGFace((pGFace)*fc,tg,prt,u);
+    vol += amsi::measureVol_pGFace((pGFace)*fc,tg,prt,u);
   int rnk = -1;
   MPI_Comm_rank(MPI_COMM_WORLD,&rnk);
   if(rnk == 0)
