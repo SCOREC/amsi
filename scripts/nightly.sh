e18344e590410c31e0071c97fbd415a16f655d75
William Tobin <tobinw2@rpi.edu>
2016-10-21 11:11:31 -0400

    more work on nightly test script

--- scripts/nightly.sh
+++ scripts/nightly.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-. /usr/local/modules
+#!/bin/bash
+. /usr/local/modules/modules-bash
 /usr/local/cmake/latest/bin/ctest -S /fasttmp/wtobin/develop/nightly/amsi/scripts/test_debug.ctest,Nightly -V
