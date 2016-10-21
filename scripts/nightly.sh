a433baf3938f899645c48a6b773664ad1aae7234
William Tobin <tobinw2@rpi.edu>
2016-10-21 11:08:04 -0400

    using sh commands for nightly since cron runs in sh, not bash

--- scripts/nightly.sh
+++ scripts/nightly.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-source /usr/local/modules
+#!/bin/sh
+. /usr/local/modules
 /usr/local/cmake/latest/bin/ctest -S /fasttmp/wtobin/develop/nightly/amsi/scripts/test_debug.ctest,Nightly -V
