58b15b662f96934ac03abb3c614302bc5772c41b
William Tobin <tobinw2@rpi.edu>
2016-11-23 12:45:20 -0500

    updating all build-system references from 'interface' to 'analysis' and renaming standard code files, replacing unused amsiAnalysis.cc/h files

--- analysis/amsiAnalysis.h
+++ analysis/amsiAnalysis.h
@@ -1,15 +1,26 @@
-#ifndef AMSI_ANALYSIS_H_
-#define AMSI_ANALYSIS_H_
-#include "amsiNonlinearAnalysis.h"
-#include <list>
+#ifndef AMSI_INTERFACE_H_
+#define AMSI_INTERFACE_H_
+#include "amsiFields.h"
+#include "amsiInterfaceConfig.h"
+#include "amsiUtil.h"
+#include "amsiMPI.h"
+#include "amsiPETScLAS.h"
+#include "apfFields.h"
+#include "simAttributes.h"
+#include "simFields.h"
 namespace amsi
 {
-  class FEAIteration : public Iteration
-  {
-  protected:
-  public:
-    FEAIteration()
-    { }
-  };
+  void interfaceInit(int argc, char ** argv, MPI_Comm cm = MPI_COMM_WORLD);
+  void interfaceFree();
+# ifdef PETSC
+  void petscInit(int argc, char ** argv, MPI_Comm cm = MPI_COMM_WORLD);
+  void petscFree();
+  extern bool use_petsc;
+# endif
+# ifdef SIM
+  void simmetrixInit(int argc, char ** argv, MPI_Comm cm = MPI_COMM_WORLD);
+  void simmetrixFree();
+  extern bool use_simmetrix;
+# endif
 }
 #endif
