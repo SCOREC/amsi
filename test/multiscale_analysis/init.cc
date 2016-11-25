62ba789d2c9dddd956096c05fe48002f95e9533e
William Tobin <tobinw2@rpi.edu>
2016-11-25 15:13:06 -0500

    preventing multiple MPI_Init() calls, adding testing for combined multiscale_analysis functionality

--- test/multiscale_analysis/init.cc
+++ test/multiscale_analysis/init.cc
@@ -3,8 +3,8 @@
 #include "amsiAnalysis.h"
 int main(int argc, char * argv[])
 {
-  amsi::initMultiscale();
-  amsi::initAnalysis();
+  amsi::initMultiscale(argc,argv);
+  amsi::initAnalysis(argc,argv);
   amsi::freeAnalysis();
   amsi::freeMultiscale();
   return 0;
