8e5e32f3dcb1d4e557e1bfb5453f40888800de4b
Jacob Merson <mersoj@rpi.edu>
2017-06-08 15:08:20 -0400

    provides interfaces to retrive analysis cases
    closes #21

--- analysis/simAnalysis.cc
+++ analysis/simAnalysis.cc
@@ -1,34 +1,14 @@
 #include "simAnalysis.h"
 #include "simAttributes.h"
+#include <vector>
 namespace amsi
 {
-  pACase getNextAnalysisCase(pGModel mdl, const std::string & cs_nm)
-  {
-    static std::vector<pACase> css;
-    static bool init = false;
-    pACase rslt = NULL;
-    if(!init)
-    {
-      amsi::getTypeCases(SModel_attManager(mdl),"analysis",std::back_inserter(css));
-      init = true;
-    }
-    if(cs_nm.empty())
-    {
-      rslt = css.back();
-      css.pop_back();
-    }
-    else
-      for(auto cs = css.begin(); cs != css.end(); ++cs)
-      {
-        char * nm = AttNode_name(*cs);
-        if(cs_nm.compare(nm) == 0)
-        {
-          rslt = *cs;
-          css.erase(cs);
-          break;
-        }
-        Sim_deleteString(nm);
-      }
-    return rslt;
+  std::vector<pACase> getAnalysisCases(pGModel mdl) {
+    std::vector<pACase> cases;
+    amsi::getTypeCases(SModel_attManager(mdl),"analysis",std::back_inserter(cases));
+    return cases;
+  }
+  pACase getAnalysisCase(pGModel mdl, std::string const & case_name) {
+    return AMAN_findCase(SModel_attManager(mdl), case_name.c_str());
   }
 }
