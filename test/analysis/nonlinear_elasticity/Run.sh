2e7b3402bc6ca94b7756a53b6d19e517ff37573f
Jacob Merson <mersoj@rpi.edu>
2017-07-17 17:12:32 -0400

    Remove run scripts
    These run scripts are out dated, and unneeded due to the use of CTest

--- test/analysis/nonlinear_elasticity/Run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-EXEDIR=$DEVROOT/amsi/build_debug/test/interface/nonlinear_elasticity
-FILEDIR=$DEVROOT/amsi/test/interface/
-
-$1 mpirun $2 -np 1 $EXEDIR/nonlinear_elastic \
-  $FILEDIR/models/cube_uniaxial_10kPa.smd \
-  $FILEDIR/meshes/cube_24_tets.sms
