a0d35621582452d281a849cb24c933a6f069da9b
William Tobin <tobinw2@rpi.edu>
2017-10-26 17:03:14 -0400

    dup the comm prior to entering the user code so they are not operating on the same comm amsi is using

--- multiscale/amsiTask.h
+++ multiscale/amsiTask.h
@@ -29,8 +29,11 @@ namespace amsi
       // don't execute of the global rank is not valid
       if(localToGlobalRank(localRank()) >= 0)
       {
+        MPI_Comm cm;
+        MPI_Comm_dup(PCU_Get_Comm(),&cm);
         PCU_Switch_Comm(task_comm);
         result +=  (*exec)(argc,argv,task_comm);
+        PCU_Switch_Comm(cm);
       }
       return result;
     };
