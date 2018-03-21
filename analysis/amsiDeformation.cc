a5414729ec05d986cc088c433f463ffcceaf6a4b
Jacob Merson <mersoj@rpi.edu>
2018-03-21 15:12:48 -0400

    add test for deformation gradient
    This test compares the computed deformation gradient against the
    analytical one for uniaxial tension, pure shear, and simple shear.

--- analysis/amsiDeformation.cc
+++ analysis/amsiDeformation.cc
@@ -1,21 +1,24 @@
 #include "amsiDeformation.h"
-namespace amsi
-{
-  const apf::Matrix3x3 eye3x3(1.0,0.0,0.0,
-                              0.0,1.0,0.0,
-                              0.0,0.0,1.0);
-  void deformationGradient(apf::Element * e, const apf::Vector3 & p, apf::Matrix3x3 & F)
+namespace amsi {
+  const apf::Matrix3x3 eye3x3(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0);
+  /*
+   * \param[in] e - mesh element with the displacements as the underlying field
+   * \param[in] p - local coordinate where the deformation gradient is
+   * \param[out] F - computed deformation gradient
+   */
+  void deformationGradient(apf::Element* e, const apf::Vector3& p,
+                           apf::Matrix3x3& F)
   {
     apf::NewArray<apf::Vector3> u;
-    apf::getVectorNodes(e,u);
+    apf::getVectorNodes(e, u);
     apf::NewArray<apf::Vector3> Ni_j;
-    apf::getShapeGrads(e,p,Ni_j);
+    apf::getShapeGrads(e, p, Ni_j);
     F = eye3x3;
     int nen = apf::countNodes(e);
-    int cmp = 3; //apf::countComponente(apf::getField(e));
+    // get vector nodes always has 3 components
+    int cmp = 3;
     for (int ii = 0; ii < cmp; ++ii)
       for (int jj = 0; jj < cmp; ++jj)
-	for (int kk = 0; kk < nen; ++kk)
-	  F[ii][jj] += u[kk][ii] * Ni_j[kk][jj];
+        for (int kk = 0; kk < nen; ++kk) F[ii][jj] += u[kk][ii] * Ni_j[kk][jj];
   }
-}
+}  // namespace amsi
