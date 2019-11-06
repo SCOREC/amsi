e4b73aeb7dfe40a72579ad92769b3ed0eed1f905
Jacob Merson <mersoj@rpi.edu>
2019-11-06 00:50:00 -0500

    update erp config script

--- scripts/config_erp.sh
+++ scripts/config_erp.sh
@@ -3,15 +3,12 @@
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_TESTS=OFF \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=0 \
-      -DCMAKE_INSTALL_PREFIX=/gpfs/u/home/PASC/PASCmrsn/scratch/install-erp/amsi  \
+      -DCMAKE_INSTALL_PREFIX=/gpfs/u/home/PASC/PASCmrsn/scratch/erp/install/amsi  \
       -DCMAKE_C_COMPILER=`which mpicc` \
       -DCMAKE_CXX_COMPILER=`which mpicxx` \
-      -DSCOREC_DIR=/gpfs/u/home/PASC/PASCmrsn/scratch/install-erp/core/lib/cmake/SCOREC/ \
+      -DSCOREC_DIR="$PUMI_ROOT/lib/cmake/SCOREC/" \
       -DPETSC_DIR="$PETSC_DIR" \
       -DPETSC_ARCH=/ \
       -DENABLE_VERBOSITY:STRING=OFF \
-      -DZOLTAN_PREFIX="$PETSC_DIR" \
-      -DPARMETIS_PREFIX="$PETSC_DIR" \
-      -DPTSCOTCH_PREFIX="$PETSC_DIR" \
-      -DBOOST_ROOT="$BOOST_INCLUDE_DIR" \
+      -DENABLE_ZOLTAN=OFF \
       /gpfs/u/home/PASC/PASCmrsn/barn/amsi
