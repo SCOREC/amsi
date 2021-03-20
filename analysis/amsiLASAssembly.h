2c8fa2ed88a716712ca5cf58fe4cd8ae19b6e257
Jacob Merson <mersoj@rpi.edu>
2021-03-20 04:49:52 -0400

    Remove various files

--- analysis/amsiLASAssembly.h
+++
@@ -1,9 +0,0 @@
-#ifndef AMSI_LAS_ASSEMBLY_H_
-#define AMSI_LAS_ASSEMBLY_H_
-#include "amsiLAS2.h"
-#include "amsiElementalSystem.h"
-namespace amsi
-{
-  void assemble(las::LasOps * ops, las::Mat * k, las::Vec * f, ElementalSystem2 * es);
-}
-#endif
