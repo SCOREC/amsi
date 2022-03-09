f0b1ef9297af2a9bd4378933e9fa17027f883ab2
Jacob Merson <jacob@jacobmerson.com>
2022-03-08 22:48:16 -0500

    Delete unused scripts and cc files

--- multiscale/amsi.h
+++
@@ -1,9 +0,0 @@
-#ifndef AMSI_H_
-#define AMSI_H_
-#include "amsiMPI.h"
-namespace amsi
-{
-  void amsiInit(int argc, char ** argv, MPI_Comm cm = MPI_COMM_WORLD);
-  void amsiFree();
-}
-#endif
--- scripts/nightly.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-. /usr/local/modules/modules-bash
-/usr/local/cmake/latest/bin/ctest -S /fasttmp/wtobin/develop/nightly/amsi/scripts/test_debug.ctest,Nightly -V
