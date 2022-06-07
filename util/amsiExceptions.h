229a851dcb543e9d3cc9af3960d37047bd6535e9
Jacob Merson <jacob@jacobmerson.com>
2022-06-06 23:41:45 -0400

    properly namespace amsi exception classes

--- util/amsiExceptions.h
+++ util/amsiExceptions.h
@@ -1,6 +1,7 @@
 #ifndef AMSI_AMSIEXCEPTIONS_H
 #define AMSI_AMSIEXCEPTIONS_H
 #include <stdexcept>
+namespace amsi {
 class mpi_error : public std::logic_error {
   public:
   mpi_error(const std::string& s) : std::logic_error(s) {}
@@ -11,4 +12,5 @@ class amsi_error : public std::logic_error {
   explicit amsi_error(const std::string& s) : std::logic_error(s) {}
   explicit amsi_error(const char* s) : std::logic_error(s) {}
 };
+}
 #endif  // AMSI_AMSIEXCEPTIONS_H
